7e9e84113a068e4de67aeca7bf92bffaf3223b71
Zachary <taylor67469@yahoo.com>
2021-04-08 18:26:03 -0500

    A LOT OF WORK

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
+var nineAm=$(".9am");
+var tenAm=$(".10am");
+var elevenAm=$(".11am");
+var twelvePm=$(".12pm");
+var onePm=$(".1pm");
+var twoPm=$(".2pm");
+var threePm=$(".3pm");
+var fourPm=$(".4pm");
+var fivePm=$(".5pm");
+var before;
+var present;
+var after;
+var times=[
+    nineAm,tenAm,elevenAm,twelvePm,onePm,twoPm,threePm,fourPm,fivePm
+];
+var i=0;
+var timet=0;
 function displaytime(){
     var datetime=moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(datetime);
+    var time=moment().format('H');
+    JSON.stringify(time);
+    timet=parseInt(time);
+    checkTime();
+    function checkTime(){
+        if(timet>9&&timet<17){
+            for(i;i<10;i++){
+                times[i].css('background-color', 'green');
+                times[i].css('color', 'white');
+            }
+        }
+        else if(timet>17){
+            for(i;i<10;i++){
+                times[i].css('background-color', 'grey');
+                times[i].css('color', 'white');
+            }
+        }
+        else{
+            times[i].css('background-color', 'red');
+        }
+    }
+
 }
 setInterval(displaytime,0);
